bedba41afa3a4c879f9e760b1ef433f8cd59951c
jiteshpubreja <jitesh.pubreja@yahoo.com>
2017-12-15 06:46:40 +0530

    Updated Readme

--- database/seeds/AddAdminUser.php
+++ database/seeds/AddAdminUser.php
@@ -15,11 +15,11 @@ class AddAdminUser extends Seeder
     {
         $user = User::create([
             'salutation' => 'Mr.',
-            'fname' => 'Jitesh',
-            'lname' => 'Pubreja',
-            'mobile' => '9463844622',
-            'landline' => '01752365478',
-            'email' => 'jitesh.pubreja@yahoo.com',
+            'fname' => 'Test',
+            'lname' => 'Service',
+            'mobile' => '9876543210',
+            'landline' => '0123456789',
+            'email' => 'test.service@server.com',
             'password' => bcrypt('123456'),
             'is_activated' => 1,
         ]);
